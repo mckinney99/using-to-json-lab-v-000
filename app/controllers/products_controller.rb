7c62272b85265ebb164e2a6d2bc934c973d4b6d4
Eric McKinney <mckinney99@gmail.com>
2018-03-14 14:57:32 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,9 +22,51 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+  class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    render plain: product.inventory > 0 ? true : false
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    Product.create(product_params)
+    redirect_to products_path
+  end
+
   def show
     @product = Product.find(params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json {
+        render json: @product.to_json(only: [:id, :name, :price, :inventory, :description])
+      }
+    end
   end
+
+  def data
+    product = Product.find(params[:id])
+    render json: ProductSerializer.serialize(product)
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :inventory, :price)
+  end
+end
 
   def data
     product = Product.find(params[:id])
